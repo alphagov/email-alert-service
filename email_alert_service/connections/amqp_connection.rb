30eb96fbd36363e7c79d72d505e69db84f20e1b8
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2014-10-30 11:05:39 +0000

    Set channel prefetch to 1
    This essentially disables prefetching of messages.  Without doing this,
    workers will eagerly fetch messages from Rabbit, potentially starving
    other workers of work.

--- email_alert_service/connections/amqp_connection.rb
+++ email_alert_service/connections/amqp_connection.rb
@@ -3,6 +3,15 @@ require "bunny"
 class AMQPConnection
   attr_reader :exchange_name, :queue_name
 
+  # Only fetch one message at a time on the channel.
+  #
+  # By default, queues will grab messages eagerly, which reduces latency.
+  # However, that also means that if multiple workers are running one worker
+  # can starve another of work.  We're not expecting a high throughput on this
+  # queue, and a small bit of latency isn't a problem, so we fetch one at a
+  # time to share the work evenly.
+  NUMBER_OF_MESSAGES_TO_PREFETCH = 1
+
   def initialize(options)
     @options = options
     @exchange_name = options.fetch(:exchange)
@@ -19,7 +28,13 @@ class AMQPConnection
   end
 
   def channel
-    @_channel ||= service.create_channel
+    @_channel ||= create_channel_and_set_prefetch
+  end
+
+  def create_channel_and_set_prefetch
+    channel = service.create_channel
+    channel.prefetch(NUMBER_OF_MESSAGES_TO_PREFETCH)
+    channel
   end
 
   def exchange
