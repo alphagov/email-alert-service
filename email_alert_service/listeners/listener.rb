15f48899131e2739ca4eccb9a6c4872744e2db78
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2018-08-07 09:44:06 +0100

    Improve logging by adding stacktrace

--- email_alert_service/listeners/listener.rb
+++ email_alert_service/listeners/listener.rb
@@ -30,7 +30,7 @@ private
   end
 
   def exit_on_exception(e, document_json, properties, delivery_info)
-    logger.info("Error processing message #{delivery_info.delivery_tag}: #{e.class} (#{e.message})")
+    logger.error("Error processing message #{delivery_info.delivery_tag}: #{e.class} (#{e.message})\n#{e.backtrace.join("\n")}")
     # Rescue any exception, not just StandardError and subclasses.
     # We want to ensure that the process exits in such a situation, so we
     # explicitly call exit() after logging the error.
