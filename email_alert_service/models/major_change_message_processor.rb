1c158e8aebe8adb02d7a6a1f64477abe903e8e5d
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-10-01 11:54:58 +0100

    Stop blacklisting travel-advice-publisher
    We want travel-advice-publisher to stop talking directly to
    email-alert-api.

--- email_alert_service/models/major_change_message_processor.rb
+++ email_alert_service/models/major_change_message_processor.rb
@@ -86,7 +86,7 @@ protected
     # These publishing apps make direct calls to email-alert-api to send their
     # emails, so we need to avoid sending duplicate emails when they come
     # through on the queue:
-    return true if %w(travel-advice-publisher specialist-publisher).include?(publishing_app)
+    return true if %w(specialist-publisher).include?(publishing_app)
 
     # These publishing apps don't manage any content where it would make sense to
     # subscribe to email updates for
