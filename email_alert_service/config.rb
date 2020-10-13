edebcb9c43b170d389af1b6c571a32bc4a09f4db
Ben Thorner <ben.thorner@digital.cabinet-office.gov.uk>
2020-10-13 13:06:44 +0100

    Remove redundant MultiIO class
    This got missed in c249e6e.

--- email_alert_service/config.rb
+++ email_alert_service/config.rb
@@ -50,18 +50,4 @@ module EmailAlertService
       obj
     end
   end
-
-  class MultiIO
-    def initialize(*targets)
-      @targets = targets
-    end
-
-    def write(*args)
-      @targets.each { |t| t.write(*args) }
-    end
-
-    def close
-      @targets.each(&:close)
-    end
-  end
 end
