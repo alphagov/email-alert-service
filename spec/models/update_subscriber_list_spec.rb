8ca4928a879ac110477ab9203a7ab76e7b17dc4d
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2022-01-21 18:00:30 +0000

    Add UpdateSubscriberList class
    WHAT
    ====
    
    The GOV.UK Accounts team added a feature where logged in users could
    subscribe to email notifications for individual pages.
    
    SubscriberLists store the titles of those pages so that we can:
    - Render a list of subscriptions in the user's manage dashboard
    - Include the title of the subscriber list in emails alerting the user
    that the page has been unpublished. (Especially as there's no-longer a
    content item we can query for title at that point).
    
    We record these titles when the SubscriberList is first made, however
    there has not been a mechanism to update it. This is a problem as
    content titles do change over time.
    
    This PR adds a class we can use to gather information from major and
    minor change messages and update the subscriber list titles using a new
    [Email Alert API endpoint](1)
    
    Other parts of email-alert-service make calls to services that use GDS
    API adaptors. However they seem not to use the test helpers that ship
    with the gem through webmock.
    
    A note on test strategy
    ======================
    
    In previous work we decided to keep the testing strategy consistent
    across the repo, and also avoided gds-api-test helpers. Instead we
    tested with double and spys.
    
    This got us into a bit of trouble when our parameters and payloads were
    not matching the gds-api-adaptors, and reminded us how helpful those
    test helpers were.
    
    So we've introduced stubs and webmock for this test, aware this
    introduces an inconsistent test strategy.
    
    We intend to go back and refactor tests for the recently introduced
    unpublication alert work. And hopefully refactor the rest of the test
    suite to.
    
    If you are far in the future and that didn't happen. Sorry! But
    hopefully you at least know why these tests don't look like the others.
    
    (1): https://github.com/alphagov/email-alert-api/pull/1704
    
    Co-authored-by: Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
    Co-authored-by: Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>

---
+++ spec/models/update_subscriber_list_spec.rb
@@ -0,0 +1,119 @@
+require "spec_helper"
+
+RSpec.describe UpdateSubscriberList do
+  let(:content_id) { SecureRandom.uuid }
+  let(:document_title) { "Example Title" }
+  let(:major_change_document) do
+    {
+      "base_path" => "/foo",
+      "content_id" => content_id,
+      "title" => document_title,
+      "update_type" => "major",
+    }
+  end
+
+  let(:minor_change_document) do
+    {
+      "base_path" => "/foo",
+      "content_id" => content_id,
+      "title" => document_title,
+      "update_type" => "minor",
+    }
+  end
+
+  let(:logger) { double(:logger, info: nil) }
+  let(:update_subscriber_list) { UpdateSubscriberList.new(document, logger) }
+  let(:subscriber_list_slug) { "subscriber_list_slug" }
+  let(:updateable_parameters) { { "title" => document_title } }
+
+  let(:subscriber_list_attributes) do
+    {
+      "content_id" => content_id,
+      "slug" => subscriber_list_slug,
+      "title" => subscriber_list_title,
+    }
+  end
+
+  describe "#trigger" do
+    shared_examples "an attempt to update subscriber lists with appropriate logging" do
+      let(:subscriber_list_title) { "An old outdated title" }
+
+      context "subscriber list found by content id" do
+        before { stub_email_alert_api_has_subscriber_list(subscriber_list_attributes) }
+
+        it "logs an attempt to update the subscriber list and triggers the api to update the title" do
+          stub_email_alert_api_has_subscriber_list(subscriber_list_attributes)
+          stub_update_subscriber_list_details(slug: subscriber_list_slug, params: updateable_parameters)
+          update_subscriber_list.trigger
+
+          expect(logger).to have_received(:info).with(
+            "Attempting to update subscriber list: #{subscriber_list_slug}, with: #{updateable_parameters}",
+          )
+        end
+
+        context "subscriber list title matches document list" do
+          let(:subscriber_list_title) { document_title }
+
+          it "logs the outcome and does not attempt an update request" do
+            stub_update_subscriber_list_details(slug: subscriber_list_slug, params: updateable_parameters)
+            update_subscriber_list.trigger
+
+            expect(logger).to have_received(:info).with(
+              "No update needed to subscriber list: #{subscriber_list_slug}",
+            )
+          end
+        end
+
+        context "update attempted with invalid parameters" do
+          let(:document_title) { "" }
+
+          before { stub_update_subscriber_list_details_unprocessible_entity(slug: subscriber_list_slug, params: updateable_parameters) }
+
+          it "logs the outcome" do
+            update_subscriber_list.trigger
+
+            expect(logger).to have_received(:info).with(
+              "email-alert-api returned unproessable entity updating subscriber list: #{subscriber_list_slug}, with: #{updateable_parameters}",
+            )
+          end
+        end
+
+        context "subscriber list cannot be found to update by slug" do
+          before { stub_update_subscriber_list_details_not_found(slug: subscriber_list_slug, params: updateable_parameters) }
+
+          it "logs the outcome" do
+            update_subscriber_list.trigger
+
+            expect(logger).to have_received(:info).with(
+              "email-alert-api cannot find subscriber list with slug #{subscriber_list_slug}",
+            )
+          end
+        end
+      end
+
+      context "subscriber list cannot be found by content id" do
+        before { stub_email_alert_api_does_not_have_subscriber_list(subscriber_list_attributes) }
+
+        it "logs the outcome and does not attempt an update request" do
+          update_subscriber_list.trigger
+
+          expect(logger).to have_received(:info).with(
+            "subscriber list not found for content id #{document['content_id']}",
+          )
+        end
+      end
+    end
+
+    describe "after a major change" do
+      let(:document) { major_change_document }
+
+      it_behaves_like "an attempt to update subscriber lists with appropriate logging"
+    end
+
+    describe "after a minor change" do
+      let(:document) { minor_change_document }
+
+      it_behaves_like "an attempt to update subscriber lists with appropriate logging"
+    end
+  end
+end
